e0aea40473ab27986715e35535651c34f4eafd91
wheatcao <wheatcao520@gmail.com>
2020-09-21 11:02:11 +0800

    init

---
+++ src/fastworkflow.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caozhaohai
+ *
+ */
+
+namespace fastworkflow;
+
+class fastworkflow{
+
+    public function __construct()
+    {
+        var_dump('ttttt');
+    }
+}
+
+
